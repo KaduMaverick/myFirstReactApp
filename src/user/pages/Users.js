266dc862707d5e036038809cbc86674dded24252
Kadu Maverick <kaduhermanni@gmail.com>
2020-05-14 02:58:08 -0300

    add Users page and components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useReducer } from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>Users Works!</h2>
-}
+  const USERS = [
+    {
+      id: "ul",
+      name: "Kadu Maverick",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1gYgKDcyhb0mxt6D6VFyuAU8Aoa0dCfZvu7HPEK4pxjGfEc4v&usqp=CAU",
+    },
+  ];
+
+  return <useReducer items={USERS} />;
+};
 
 export default Users;
